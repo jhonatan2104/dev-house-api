af818f183944cb71d63de9e2242d1081911d275b
Jhonatan Andrade <xdjhonatan0@gmail.com>
2020-08-09 20:49:24 -0300

    chore: add crypto-js

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,14 +1,20 @@
 import multer from 'multer';
 import path from 'path';
+import crypto from 'crypto-js'
 
 export default {
   storage: multer.diskStorage({
       destination: path.resolve(__dirname, '..', '..', 'uploads'),
       filename: (req, file, cb) => {
-        const ext = path.extname(file.originalname);
-        const name = path.basename(file.originalname, ext);
+        try {
+          const hash = crypto.lib.WordArray.random(16);
+          const ext = path.extname(file.originalname);
+          const name = path.basename(file.originalname, ext);
 
-        cb(null, `${name}-${Date.now()}${ext}`);
+          cb(null, `${name}-${hash}${ext}`);
+        } catch (error) {
+          cb(error);
+        }
       }
   }),
 }
